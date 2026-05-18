027266f86c4d5e8fe43e51baf68b135a3ba739e1
TomMarinAriza <t.marin@utp.edu.co>
2026-05-18 13:36:55 -0500

    Fixing Animations

--- DungeonAnticheat/Assets/PlayerScripts/PlayerMovement.cs
+++ DungeonAnticheat/Assets/PlayerScripts/PlayerMovement.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement stats")]
     float Speed = 5f;
 
+
     Rigidbody2D rb;
+
+    [Header("Animation")]
+
+    Animator animator;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
+        
         float moveY = Input.GetAxis("Vertical");
+        Debug.Log("MoveX: " + moveX + " MoveY: " + moveY);
 
         rb.velocity = new Vector2(moveX * Speed, moveY * Speed);
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+    
+        
+        if (moveX == 0 && moveY == 0)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            animator.SetBool("isMoving", true);
+        }
     }
 }
