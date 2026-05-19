09f3ceeff746d3ae42a662731c4720a040d1e7d8
TomMarinAriza <t.marin@utp.edu.co>
2026-05-18 21:28:39 -0500

    Adding menuScene

---
+++ DungeonAnticheat/Assets/UIcontroller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIcontroller : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
